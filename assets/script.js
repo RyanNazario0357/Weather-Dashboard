34e4c5fc42101af1954199789c5f12a2040673fc
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-21 22:51:03 -0400

    added event

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,19 @@ var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
 
 //weather data
-var apiKey = 2d80a4c01e2b180a34938f07bd6f6783
-var name = document.querySelector(.'name');
+var apiKey = "2d80a4c01e2b180a34938f07bd6f6783";
+var name = document.querySelector('.name');
 var wind = docuument.querySelector('.wind');
 var temp = document.querySelector(',temp');
 var uvindex = document.querySelector('.unindex');
 var humidity = document.querySelector('.humidity');
 var searchInput = "";
 var currentDate = moment().format("YYYY/M/D");
+
+
+button.addEventListener('click', function() {
+    console.log("button clicked")
+    searchInput = $(".inputValue").val().trim();
+    getLocation(searchInput);
+});
+
