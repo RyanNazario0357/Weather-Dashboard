454ebd618cc57345c5cb81f6adcd59872d460866
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-21 22:28:51 -0400

    added uvindex, humidity, searchinput, and current date variable

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,7 @@ var apiKey = 2d80a4c01e2b180a34938f07bd6f6783
 var name = document.querySelector(.'name');
 var wind = docuument.querySelector('.wind');
 var temp = document.querySelector(',temp');
+var uvindex = document.querySelector('.unindex');
+var humidity = document.querySelector('.humidity');
+var searchInput = "";
+var currentDate = moment().format("YYYY/M/D");
