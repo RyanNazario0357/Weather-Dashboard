7773ffa45a7aed793e3ac5ac219c9dd9ae0785b3
RyanNazario0357 <Ryan0357@icloud.com>
2022-07-21 22:23:22 -0400

    added wind,name, and temp var

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 //City Information
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
+
+//weather data
+var apiKey = 2d80a4c01e2b180a34938f07bd6f6783
+var name = document.querySelector(.'name');
+var wind = docuument.querySelector('.wind');
+var temp = document.querySelector(',temp');
